70d0fa943c9acaa3cf5be776c8c17223f3b42a17
BYUNGMIN CHOI <nekarakube@gmail.com>
2023-01-22 14:03:50 +0900

    add controller with baseUrl

--- controller.js
+++ controller.js
@@ -1,11 +1,13 @@
 import http from './http.js';
 
-function test() {
-    http.test();
-    window.console.log('controller test');
-    return 'b';
+const baseUrl = 'https://api.adviceslip.com/advice';
+
+const getRandomList = async () => {
+    const { advice, id } = await http.get(baseUrl);
+
+    return { advice, id };
 }
 
 export default {
-    test
+    getRandomList
 }
