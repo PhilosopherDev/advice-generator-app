52bb061ce2711596f31016f08e5d9da2ee389d97
BYUNGMIN CHOI <nekarakube@gmail.com>
2023-01-22 14:04:40 +0900

    remove and add title id and quote content by node control, add initail loading function

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 import controller from './controller.js';
 
-document.querySelector('button').addEventListener('click', () => {
-    main();
+const onDiceClick = async () => {
+    const { advice, id } = await controller.getRandomList();
+
+    const title = document.querySelector('.title');
+    const titleId = document.querySelector('.title span');
+    const newTitleId = document.createElement('span');
+    const titleIdText = document.createTextNode(`Advice #${id}`);
+
+    const quote = document.querySelector('.quote');
+    const quoteContent = document.querySelector('.quote q');
+    const newQuoteContent = document.createElement('q');
+    const quoteText = document.createTextNode(`“${advice}”`);
+
+    // remove and add title id
+    title.removeChild(titleId);
+    newTitleId.appendChild(titleIdText);
+    title.appendChild(newTitleId);
+
+    //  remove and add quote content
+    quote.removeChild(quoteContent);
+    newQuoteContent.appendChild(quoteText);
+    quote.appendChild(newQuoteContent);
+};
+
+document.querySelector('.dice').addEventListener('click', onDiceClick)
+
+window.addEventListener('load', () => {
+    onDiceClick();
 })
-
-function main() {
-    const a = controller.test();
-
-    console.log('index.js', { a });
-}
-
-export default {
-    main
-}
